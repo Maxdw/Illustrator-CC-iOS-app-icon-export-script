9e0e1fc99081363fdeb01da8a42af747d63fa152
Maxdw <max@dutchwise.nl>
2016-06-05 15:06:07 +0200

    Initial commit

---
+++ app.icon.jsx
@@ -0,0 +1,219 @@
+var AppIconResizeClass = function(name, sizes) {
+
+    var Self = this;
+
+    var ext = "png";
+
+    this.document = app.activeDocument;
+
+    this.folder = null;
+
+    this.artboard = null;
+
+    this.parseResizeDirective = function(size) {
+        if (!size.size || (size.size && size.size.split('x').length != 2)) {
+            return false;
+        }
+
+        var width = size.size.split('x')[0],
+            height = size.size.split('x')[1],
+            scale = 1;
+
+        if (size.scale) {
+            scale = parseInt(size.scale.replace('x', ''));
+        }
+
+        var directive = {
+            filename: "",
+            scaleFactorWidth: this.getScaleFactorWidth(width * scale),
+            scaleFactorHeight: this.getScaleFactorHeight(height * scale)
+        };
+
+        if (!size.idiom) {
+            size.idiom = "unknownidiom";
+        }
+
+        if (size.scale && size.scale == "1x") {
+            directive.filename = name + "." + size.idiom + size.size + "." + ext;
+        }
+        else if(size.scale) {
+            directive.filename = name + "." + size.idiom + size.size + "@" + size.scale + "." + ext;
+        }
+        else {
+            directive.filename = "unknownscale" + ext;
+        }
+
+        return directive;
+    };
+
+    this.setUserRequestedFolder = function() {
+        this.folder = Folder.selectDialog();
+    };
+
+    this.setActiveArtboard = function() {
+        if(!this.document) {
+            return false;
+        }
+
+        var document = this.document,
+            index = document.artboards.getActiveArtboardIndex();
+    
+        if(index != undefined && document.artboards[index]) {
+            this.artboard = document.artboards[index];
+            return true;
+        }
+    
+        return false;
+    };
+
+    this.getScaleFactorWidth = function(newWidth) {
+        if (!this.artboard) {
+            return 100;
+        }
+
+        var scaleFactor,
+            oldWidth = ( this.artboard.artboardRect[2] - this.artboard.artboardRect[0] );
+        
+        return ( newWidth / oldWidth ) * 100;
+    };
+
+    this.getScaleFactorHeight = function(newHeight) {
+        if (!this.artboard) {
+            return 100;
+        }
+
+        var scaleFactor,
+            oldHeight = ( this.artboard.artboardRect[1] - this.artboard.artboardRect[3] );
+        
+        return ( newHeight / oldHeight ) * 100;
+    };
+
+    this.exportFileToPNG24 = function(filepath, scaleFactorWidth, scaleFactorHeight) {  
+        var options = new ExportOptionsPNG24(),
+            type = ExportType.PNG24,
+            file = new File(filepath);
+
+        options.antiAliasing = true;
+        options.transparency = true;
+        options.saveAsHTML = false;
+        options.artBoardClipping = true;
+        options.horizontalScale = scaleFactorWidth;
+        options.verticalScale = scaleFactorHeight;
+
+        this.document.exportFile(file, type, options);
+        return true;
+    };
+
+    this.saveAll = function() {
+        if (!this.folder || !this.artboard || !this.document) {
+            return false;
+        }
+
+        var results = [];
+
+        for (var i = 0, directive, result; i < sizes.length; i++) {
+            directive = this.parseResizeDirective(sizes[i]);
+
+            if (!directive) {
+                results.push(false);
+                continue;
+            }
+
+            try {
+                result = this.exportFileToPNG24(this.folder.fsName + "/" + directive.filename, directive.scaleFactorWidth, directive.scaleFactorHeight);
+                results.push(result);
+            }
+            catch(e) {
+                alert(e);
+            }
+        }
+
+        for (var ri = 0; ri < results.length; ri++) {
+            if (!results[ri]) {
+                return false;
+            }
+        }
+
+        return true;
+    };  
+
+};
+
+
+var icon_resizer = new AppIconResizeClass("app.icon", [
+    {
+      "size" : "29x29",
+      "idiom" : "iphone",
+      "scale" : "2x"
+    },
+    {
+      "size" : "29x29",
+      "idiom" : "iphone",
+      "scale" : "3x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "iphone",
+      "scale" : "2x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "iphone",
+      "scale" : "3x"
+    },
+    {
+      "size" : "60x60",
+      "idiom" : "iphone",
+      "scale" : "2x"
+    },
+    {
+      "size" : "60x60",
+      "idiom" : "iphone",
+      "scale" : "3x"
+    },
+    {
+      "size" : "29x29",
+      "idiom" : "ipad",
+      "scale" : "1x"
+    },
+    {
+      "size" : "29x29",
+      "idiom" : "ipad",
+      "scale" : "2x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "ipad",
+      "scale" : "1x"
+    },
+    {
+      "size" : "40x40",
+      "idiom" : "ipad",
+      "scale" : "2x"
+    },
+    {
+      "size" : "76x76",
+      "idiom" : "ipad",
+      "scale" : "1x"
+    },
+    {
+      "size" : "76x76",
+      "idiom" : "ipad",
+      "scale" : "2x"
+    },
+    {
+      "size" : "83.5x83.5",
+      "idiom" : "ipad",
+      "scale" : "2x"
+    }
+]);
+
+icon_resizer.setUserRequestedFolder();
+icon_resizer.setActiveArtboard();
+
+if(icon_resizer.saveAll()) {
+    alert("Saved app icons to: " + icon_resizer.folder.fsName);
+}
+else {
+    alert("Saving failed or possibly partially failed...");
+}
---
+++ readme.md
@@ -0,0 +1,38 @@
+Illustrator CC iOS app icon export script
+======================
+This script will export your active artboard to all retina and non-retina app icon sizes in .png format. The sizes are obtained from a hard-coded JSON/Javascript array that originates from XCode's Contents.json that is generated when creating a Appicon asset in XCode.
+
+----------
+
+## Download
+[Download ZIP](https://github.com/Maxdw/Illustrator-CC-iOS-app-icon-export-script/archive/master.zip)
+
+## Usage
+** Create a square artboard of 1024x1024 pixels (also works with other sizes)
+** Design your app icon
+** Run app.icon.jsx in Illustrator with your app icon artboard active (shortcut ⌘+12)
+** Select your output folder
+** Place the .png files in your Xcode's Appicon assets at the appropriate spots
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2016 Dutchwise V.O.F. (http://dutchwise.nl)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
